a025452d1ee69f0117172354230396c91d37792b
DESKTOP-PVVSJ3E\kosmo <fins6789@gmail.com>
2023-05-03 09:38:17 +0900

    0503 intellij spring

---
+++ shop/shop/src/main/java/com/shop/dto/ItemDto.java
@@ -0,0 +1,21 @@
+package com.shop.dto;
+
+import com.shop.constant.ItemSellStatus;
+import lombok.Data;
+
+import javax.persistence.Column;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.Lob;
+import java.time.LocalDateTime;
+
+@Data
+public class ItemDto {
+    private Long id;
+    private String itemNm;
+    private int price;
+    private String itemDetail;
+    private String sellStatCd;
+    private LocalDateTime regTime;
+    private LocalDateTime updateTime;
+}
